aa75b16decf69e8934492c35747a8893624bcdbd
yezixbei <yezixbei@gmail.com>
2019-06-18 16:11:12 -0700

    trying to connect to remote database

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,50 +1,44 @@
 const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1/BobaBae'
-const dbURL = `mongodb://${host}`;
-const readLine = require('readline');
-
-const connect = () => {
-  setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true }), 1000);
+let dbURI = 'mongodb://localhost/BobaBae';
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGODB_URI;
 }
-
+mongoose.connect(dbURI, { useNewUrlParser: true });
 
 mongoose.connection.on('connected', () => {
-  console.log('connected');
+  console.log(`Mongoose connected to ${dbURI}`);
 });
-
 mongoose.connection.on('error', err => {
-  console.log('error: ' + err);
-  return connect();
+  console.log('Mongoose connection error:', err);
 });
-
 mongoose.connection.on('disconnected', () => {
-  console.log('disconnected');
+  console.log('Mongoose disconnected');
 });
-
 
 const gracefulShutdown = (msg, callback) => {
-  mongoose.connection.close( () => {
+  mongoose.connection.close(() => {
     console.log(`Mongoose disconnected through ${msg}`);
     callback();
   });
 };
 
+// For nodemon restarts                                 
 process.once('SIGUSR2', () => {
   gracefulShutdown('nodemon restart', () => {
     process.kill(process.pid, 'SIGUSR2');
   });
 });
+// For app termination
 process.on('SIGINT', () => {
   gracefulShutdown('app termination', () => {
     process.exit(0);
   });
 });
+// For Heroku app termination
 process.on('SIGTERM', () => {
   gracefulShutdown('Heroku app shutdown', () => {
     process.exit(0);
   });
 });
-
-connect();
 
 require('./locations');
